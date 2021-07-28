f6659ad462d30e91041637703be1f62a0a5a2da4
jawon <agyh8491@gmail.com>
2021-07-28 23:18:16 +0900

    jaone

--- app/src/main/java/com/example/movie/MainActivity.java
+++ app/src/main/java/com/example/movie/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.movie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button imageButton = (Button) findViewById(R.id.btn1);
+        imageButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(getApplicationContext(), PhotoActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton2 = (Button) findViewById(R.id.btn2);
+        imageButton2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(getApplicationContext(), DiaryActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton3 = (Button) findViewById(R.id.btn3);
+        imageButton3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(getApplicationContext(), aMovieActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
